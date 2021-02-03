03e2adb50c753aa7a2433cb0923e0a288cc900b8
hzeraat2 <>
2021-02-03 11:20:57 +0000

    React native navigation added and basic navigation flow set up.

--- App.js
+++ App.js
@@ -1,88 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Home from './components/Home';
+import NewsArticles from './components/NewsArticles';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const AppNavigator = createStackNavigator(
+  {
+    MainScreen: Home,
+    NewsArticles: NewsArticles
+  },
+  {
+    initialRouteName: "MainScreen"
+  },
+  {
+    headerMode: "none",
+    navigationOptions: {
+      headerVisible: false
+    },
+    transitionConfig: () => fadeIn()
+  }
+);
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
- 
-const App: () => React$Node = () => {
-
-  fetch('https://apps-tests.s3-eu-west-1.amazonaws.com/react-native/articles.json')
-  .then(response => response.json())
-  .then(data => console.log( data ))
-  .catch(error => console.log(error));
-
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-      
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+export default createAppContainer(AppNavigator);
---
+++ components/Home.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import {
+  View,
+  Text,
+
+} from 'react-native';
+
+
+// fetch('https://apps-tests.s3-eu-west-1.amazonaws.com/react-native/articles.json')
+//   .then(response => response.json())
+//   .then(data => console.log( data ))
+//   .catch(error => console.log(error));
+const Home = () => {
+  return (
+    <View>
+      <Text style={{ color: "red" }}>Home Screen</Text>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+export default Home
